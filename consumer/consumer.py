1623f0e1240230f02076e554f3b97b806c0ed2a5
KosmosoJ <darko.donie3@gmail.com>
2025-02-04 09:34:22 +0500

    add dlq

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -3,23 +3,25 @@ from aiokafka.errors import KafkaConnectionError
 import asyncio
 import json
 import os
-from utils import db_get_users, db_add_notification, db_read_notification
+from utils import db_get_users, db_add_notification, db_read_notification, send_to_dlq
 
 
 KAFKA_URL = os.getenv("KAFKA_URL")
 
 
 async def add_response(body, *args, **kwargs):
-    """ Функция сохранения респонса в кафку """
+    """Функция сохранения респонса в кафку"""
     data = {
         "request_id": body["request_id"],
         "message": {
-            "action": body['message']['action'],
+            "action": body["message"]["action"],
             "body": {"message": "success"},
         },
     }
 
-    data['message']['body'].update(kwargs['data'])
+    data["message"]["body"].update(
+        kwargs["data"]
+    )  # Добавление к респонсу информации о запросе
 
     producer = AIOKafkaProducer(bootstrap_servers=KAFKA_URL)
     await producer.start()
@@ -29,24 +31,29 @@ async def add_response(body, *args, **kwargs):
     )
     await producer.stop()
 
+
 async def get_users(message):
-    """ Получение всех пользователей (вывод просто принтом стоит) """
+    """Получение всех пользователей (вывод просто принтом стоит)"""
     users = await db_get_users(message["message"]["body"])
-    await add_response(message, data={'users':users})
-    #TODO Сделать вывод в request_response
+    await add_response(message, data={"users": users})
+
 
 async def add_notifications(body):
-    """ Создание уведомлений в монго и сохранение респонса в кафку """
+    """Создание уведомлений в монго и сохранение респонса в кафку"""
     id = await db_add_notification(body)
     await add_response(body, data=id)
 
+
 async def edit_document(body):
-    """ Редактирование и сохранение реквеста в монго"""
+    """Редактирование и сохранение реквеста в монго"""
     notification = await db_read_notification(body)
-    await add_response(body, data={'notification_id':body['message']['body']['notification_id']})
+    await add_response(
+        body, data={"notification_id": body["message"]["body"]["notification_id"]}
+    )
+
 
 async def process_data(action, body=None):
-    """ Фильтр на команды """
+    """Фильтр на команды"""
     actions = {
         "add_notification": add_notifications,
         "get_users": get_users,
@@ -55,11 +62,11 @@ async def process_data(action, body=None):
     if action in actions:
         return await actions[action](body) if actions[action] else None
     else:
-        ...  # TODO Сделать выброс в DLQ
+        await send_to_dlq(json.dumps(body).encode("utf-8"))
 
 
 async def consume_data():
-    """ Получение данных из кафки """
+    """Получение данных из кафки"""
     consumer = AIOKafkaConsumer(
         "notification_requests",
         group_id="data_consumber",
@@ -71,8 +78,8 @@ async def consume_data():
         async for msg in consumer:
             raw_data = msg.value.decode("utf-8")
             msg_data = json.loads(raw_data)
-            if msg_data["message"]["action"] is None:#Если нет действия то принт #TODO сделать выброс в DLQ
-                print(msg_data)
+            if msg_data["message"]["action"] is None    :
+                await send_to_dlq(msg)
             await process_data(action=msg_data["message"]["action"], body=msg_data)
 
     finally:
--- consumer/utils.py
+++ consumer/utils.py
@@ -1,23 +1,22 @@
 from db import app
-import pymongo
 from datetime import datetime
-from bson import ObjectId, json_util
-import json 
+from bson import ObjectId
+from aiokafka import AIOKafkaProducer
+import os
+
+KAFKA_URL = os.getenv("KAFKA_URL")
+
 
 async def db_get_users(body=None):
-    """ Получение всех юзеров из монго """
-    client = pymongo.MongoClient("mongodb://root:example@mongo:27017/")
-    raw_users = client.app.users.find()
+    """Получение всех юзеров из монго"""
+    raw_users = app.users.find()
     users = list(raw_users)
-    # for raw_user in raw_users:
-    #     raw_user['_id'] = str(raw_user['_id'])
-    #     users.append(raw_user)
-    
+
     return users
 
 
 async def db_add_notification(message):
-    """ Добавление нового уведомления в монго"""
+    """Добавление нового уведомления в монго"""
     body = message["message"]["body"]
     data = {
         "user_id": str(body["user_id"]),
@@ -36,16 +35,27 @@ async def db_add_notification(message):
             "type": body["metadata"]["type"],
         },
     }
-    client = pymongo.MongoClient("mongodb://root:example@mongo:27017/")
-    users = client.app.notifications.insert_one(data).inserted_id
+    users = app.notifications.insert_one(data).inserted_id
     return users
 
 
 async def db_read_notification(message):
-    """ Запись новой информации в уведомления"""
-    body = message['message']['body']
-    update_data = {'$set':{'is_read':True, 'read_at':datetime.now()}}
-    notification = app.notifications.update_one(filter={'_id': ObjectId(body['notification_id'])},update=update_data)
-    
+    """Запись новой информации в уведомления"""
+    body = message["message"]["body"]
+    update_data = {"$set": {"is_read": True, "read_at": datetime.now()}}
+    notification = app.notifications.update_one(
+        filter={"_id": ObjectId(body["notification_id"])}, update=update_data
+    )
+
     return str(notification)
-    
+
+
+async def send_to_dlq(message):
+    """ Отправка сообщения в мертвую очередь """
+    producer = AIOKafkaProducer(bootstrap_servers=KAFKA_URL)
+    await producer.start()
+
+    try:
+        await producer.send_and_wait("dead_letter",value=message)
+    finally:
+        await producer.stop()
